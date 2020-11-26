268edbd7d85df4d7f49ecf55cf7cefb5b13cf0f5
Ghadi14 <68791415+Ghadi14@users.noreply.github.com>
2020-11-26 06:14:55 +0200

    Add files via upload

---
+++ dogs_cats.py
@@ -0,0 +1,92 @@
+
+
+from google.colab import drive
+drive.mount('/content/drive/')
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import cv2
+from tqdm import tqdm
+
+DATADIR="/content/drive/MyDrive/Untitled folder/PetImages"
+CATEGORIES=["Dog","Cat"]
+training_data = []
+IMG_SIZE=50
+
+def create_training_data():
+    for category in CATEGORIES:  
+
+        path = os.path.join(DATADIR,category)  
+        class_num = CATEGORIES.index(category)  
+
+        for img in tqdm(os.listdir(path)): 
+            try:
+                img_array = cv2.imread(os.path.join(path,img) ,cv2.IMREAD_GRAYSCALE)  
+                new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE)) 
+                training_data.append([new_array, class_num])  
+            except Exception as e:  
+                pass
+
+create_training_data()
+
+X = []
+y = []
+
+for features,label in training_data:
+    X.append(features)
+    y.append(label)
+
+print(X[0].reshape(-1, IMG_SIZE, IMG_SIZE, 1))
+
+X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+
+import pickle
+
+pickle_out = open("X.pickle","wb")
+pickle.dump(X, pickle_out)
+pickle_out.close()
+
+pickle_out = open("y.pickle","wb")
+pickle.dump(y, pickle_out)
+pickle_out.close()
+
+pickle_in = open("X.pickle","rb")
+X = pickle.load(pickle_in)
+
+pickle_in = open("y.pickle","rb")
+y = pickle.load(pickle_in)
+
+X = X/255.0
+
+model = Sequential()
+
+model.add(Conv2D(256, (3, 3), input_shape=X.shape[1:]))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(256, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())  
+model.add(Dense(64))
+
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+model.fit(X, y, batch_size=32, epochs=3, validation_split=0.3)
+
+def prepare(filepath):
+    IMG_SIZE = 50
+    img_array = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)  
+    new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))  
+    return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+
+prediction = model.predict([prepare('dog.jpg')])
+print(prediction)  
+print(CATEGORIES[int(prediction[0][0])])
